e9a9992a848b2200db2153b3342f2614b526d974
Miguel Angel Alarcos Torrecillas <miguelangel.alarcos@proton.me>
2025-05-19 15:12:09 +0200

    added trpc y html5-qrcode

---
+++ src/hooks.server.ts
@@ -0,0 +1,47 @@
+import type { Handle } from '@sveltejs/kit';
+import { paraglideMiddleware } from '$lib/paraglide/server';
+import { sequence } from '@sveltejs/kit/hooks';
+//import { startListening } from '$lib/mpdListener';
+//import { initializeDB } from '$lib/db';
+import { createTRPCHandle } from 'trpc-sveltekit';
+import { createContext } from '$lib/trpc/context';
+import { router } from '$lib/trpc/router';
+//import { appRouter } from '$lib/trpc/router';
+
+const trpcHandle: Handle = createTRPCHandle({ router, createContext });
+
+const handleParaglide: Handle = ({ event, resolve }) =>
+	paraglideMiddleware(event.request, ({ request, locale }) => {
+		event.request = request;
+
+		return resolve(event, {
+			transformPageChunk: ({ html }) => html.replace('%paraglide.lang%', locale)
+		});
+	});
+
+let hasInitialized = false;
+
+async function initializeServer() {
+  console.log("Inicializando el servidor...");
+
+  try{
+    //await initializeDB(); 
+    // Inicializar la escucha de eventos MPD
+    //await startListening();
+    console.log("Servidor inicializado.");
+  }catch(e){
+    console.error(e);
+  }
+  
+  hasInitialized = true;
+}
+
+const defaultHandle: Handle = async ({ event, resolve }) => {
+  if (!hasInitialized) {
+    await initializeServer();
+  }
+  return await resolve(event);
+}
+
+export const handle: Handle = sequence(handleParaglide, defaultHandle, trpcHandle);
+
---
+++ src/lib/trpc/context.ts
@@ -0,0 +1,10 @@
+import type { RequestEvent } from '@sveltejs/kit';
+
+
+export async function createContext(event: RequestEvent) {
+  return {
+    event, // 👈 `event` is now available in your context,
+  };
+}
+
+export type Context = Awaited<ReturnType<typeof createContext>>;
